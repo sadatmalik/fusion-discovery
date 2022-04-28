5e665e9ae8935e6f8cfbf2ea566cf79eb823a1df
sadat.malik <sadat.malik@me.com>
2022-04-28 14:39:32 +0100

    initial spring.io files

---
+++ src/main/java/com/sadatmalik/fusiondiscovery/fusiondiscovery/FusionDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.fusiondiscovery.fusiondiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusionDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusionDiscoveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadatmalik/fusiondiscovery/fusiondiscovery/FusionDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadatmalik.fusiondiscovery.fusiondiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusionDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
