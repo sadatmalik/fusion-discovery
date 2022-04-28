8cb74e3441ce90cb54b86e86d8159a53404819f3
sadat.malik <sadat.malik@me.com>
2022-04-28 14:55:58 +0100

    docker file, maven dependencies, and package renaming

--- src/main/java/com/sadatmalik/fusiondiscovery/FusionDiscoveryApplication.java
+++ src/main/java/com/sadatmalik/fusiondiscovery/FusionDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.sadatmalik.fusiondiscovery.fusiondiscovery;
+package com.sadatmalik.fusiondiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sadatmalik/fusiondiscovery/FusionDiscoveryApplicationTests.java
+++ src/test/java/com/sadatmalik/fusiondiscovery/FusionDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sadatmalik.fusiondiscovery.fusiondiscovery;
+package com.sadatmalik.fusiondiscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
