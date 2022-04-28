f49aa12dd93f81e8875510f91aed28f571403d66
sadat.malik <sadat.malik@me.com>
2022-04-28 14:46:31 +0100

    fusion discovery configuration

--- src/main/java/com/sadatmalik/fusiondiscovery/fusiondiscovery/FusionDiscoveryApplication.java
+++ src/main/java/com/sadatmalik/fusiondiscovery/fusiondiscovery/FusionDiscoveryApplication.java
@@ -2,8 +2,15 @@ package com.sadatmalik.fusiondiscovery.fusiondiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @EnableEurekaServer enables the application to start up as a service discovery server.
+ *
+ * @author sadatmalik
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class FusionDiscoveryApplication {
 
 	public static void main(String[] args) {
